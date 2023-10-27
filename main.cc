54c6756706acbe8c0b77ada7f34895e9da1c137a
Sabina Abdiganieva <abdiganieva.sb@phystech.edu>
2023-10-27 22:55:20 +0300

    add ray and simple camera

--- main.cc
+++ main.cc
@@ -1,12 +1,48 @@
 #include <iostream>
 #include "vec3.h"
+#include "color.h"
+#include "ray.h"
+
+
+#include <cassert>
+
+color ray_color(const ray &r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	assert( std::abs(unit_direction.length() - 1.0) < 1e-15 );
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 0.0, 0.3) + a*color(0.8, 0.0, 1.0);
+}
 
 int main() {
 
-/*	// Image
-	int image_width = 256;
-	int image_height = 256;
-	
+	// Image
+	auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 400;
+
+	// Calculating image height using aspect ratio and ensuring that it's at least 1
+	int image_height = static_cast<int>(image_width / aspect_ratio);
+	image_height = (image_height < 1) ? 1 : image_height;
+
+	//Camera
+	auto focal_length = 1.0;
+	auto viewport_height = 2.0;
+	auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+	auto camera_center = point3(0, 0, 0);
+
+	auto viewport_u = vec3(viewport_width, 0, 0);
+	auto viewport_v = vec3(0, -viewport_height, 0);
+
+	// Horizontal and vertical delta pixels (distance between 2 pixels)
+	auto u_delta = viewport_u / image_width;
+	auto v_delta = viewport_v / image_height;
+
+	// Viewport upper left pixel location
+	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+
+	// Center of upper left pixel
+	auto pixel00_loc = viewport_upper_left + 0.5 * (u_delta + v_delta);
+
+
 	// Render
 	
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -14,19 +50,14 @@ int main() {
 	for (int j = 0; j < image_height; ++j) {
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0;
 			
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-			
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			auto pixel_center_loc = pixel00_loc + (i * u_delta) + (j * v_delta);
+			auto ray_direction = pixel_center_loc - camera_center;
+			ray r = ray(camera_center, ray_direction);
+
+			color pixel_color = ray_color(r);
+			write_color(std::cout, pixel_color);
 		}
 	}
-	*/
-	vec3 a(1, 2, 3);
-	vec3 b(4, 5, 6);
-	std::cout << a - b;
+	std::clog << "All done!" << '\n';
 }
--- vec3.h
+++ vec3.h
@@ -45,11 +45,15 @@ class vec3 {
 		}
 		
 		double length() const {
-			return sqrt(length_squared());
+			return std::sqrt(length_squared());
+		}
+
+		double dot(const vec3& u) const {
+			return e[0] * u.e[0] + e[1] * u.e[1] + e[2] * u.e[2];
 		}
 		
 		double length_squared() const {
-			return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+			return dot(*this);
 		}
 };
 
@@ -67,8 +71,20 @@ inline vec3 operator-(vec3 u, const vec3 &v) {
 	return (u -= v);
 }
 
-inline vec3 operator*(const vec3 v, double t) {
+inline vec3 operator*(const vec3 &v, double t) {
 	return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+	return v * t;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+	return v /= t;
+}
+
+inline vec3 operator*(const vec3 &v, const vec3 &u) {
+	return vec3(v.e[0] * u.e[0], v.e[1] * u.e[1], v.e[2] * u.e[2]);
 }
 
 inline vec3 unit_vector(const vec3 v) {
@@ -76,6 +92,13 @@ inline vec3 unit_vector(const vec3 v) {
 }
 
 inline double dot(const vec3 v, const vec3 u) {
-	return v.e[0] * u.e[0] + v.e[1] * u.e[1] + v.e[2] * u.e[2];
+	return v.dot(u);
 }
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
 #endif
