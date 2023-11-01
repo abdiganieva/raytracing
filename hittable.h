bfe79c4d4d6f9cf0317df953972310fb9fc1b9dd
Sabina Abdiganieva <abdiganieva.sb@phystech.edu>
2023-11-02 02:23:40 +0300

    add abstract class for hittable objects, front-face tracking

--- hittable.h
+++ hittable.h
@@ -3,6 +3,25 @@
 
 #include "ray.h"
 
-class hittable {
-    
+class hit_record {
+    public:
+        point3 p;
+        vec3 normal;
+        double t;
+        bool front_face;
+
+        // Sets the hit record normal vector. The parameter `outward_normal` is assumed to have unit length
+        void set_face_normal(const ray &r, const vec3 &outward_normal) {
+            this->front_face = r.direction().dot(outward_normal) < 0;
+            this->normal = this->front_face ? outward_normal : - outward_normal; 
+        }
 }
+
+class hittable {
+    public:
+        virtual ~hittable() = default;
+
+        virtual bool hit(ray &r, double r_tmin, double r_tmax, double r_tmin, hit_record &rec) const = 0;
+};
+
+#endif
--- sphere.h
+++ sphere.h
@@ -1,4 +1,41 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
-class sphere
+#include "vec3.h"
+#include "ray.h"
+
+class sphere : public hittable {
+
+    public:
+        sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
+
+    private:
+        point3 center;
+        double radius;
+
+    bool hit(const ray &r, double r_tmin, double r_tmax, double r_tmin, hit_record &rec) const override {
+
+        vec3 oc = r.origin() - this->center;
+	    auto a = r.direction().length_squared();
+	    auto half_b = oc.dot(r.direction());
+	    auto c = oc.length_squared() - this->raduis * this->raduis;
+	    auto discriminant = half_b *half_b - a * c;
+	    if (discriminant < 0) return false;
+        auto sqrt_discr = std::sqrt(discriminant);
+
+        // Finding nearest root t that lies in the range t_min < t < t_max
+        auto root = (-half_b - sqrt_discr) / a;
+        if (root <= r_tmin || root >= r_tmax) {
+            root = (-half_b + sqrt_discr) / a;
+            if (root <= r_tmin || root >= r_tmax) return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - this->center) / this->radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+    
+}
